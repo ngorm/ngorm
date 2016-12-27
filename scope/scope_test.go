e9f8583a99204e8a620979825ce81c296efd0fd3
gernest <geofreyernest@live.com>
2016-12-27 10:53:06 +0300

    [scope] Add TestFieldByName

--- scope/scope_test.go
+++ scope/scope_test.go
@@ -1,1 +1,45 @@
 package scope
+
+import (
+	"testing"
+
+	"github.com/gernest/gorm/engine"
+	"github.com/gernest/gorm/model"
+)
+
+type CalculateField struct {
+	model.Model
+	Name     string
+	Children []CalculateFieldChild
+	Category CalculateFieldCategory
+	EmbeddedField
+}
+
+type EmbeddedField struct {
+	EmbeddedName string `sql:"NOT NULL;DEFAULT:'hello'"`
+}
+
+type CalculateFieldChild struct {
+	model.Model
+	CalculateFieldID uint
+	Name             string
+}
+
+type CalculateFieldCategory struct {
+	model.Model
+	CalculateFieldID uint
+	Name             string
+}
+
+func TestFieldByName(t *testing.T) {
+	e := &engine.Engine{
+		Search:    &engine.Search{},
+		Scope:     &engine.Scope{},
+		StructMap: model.NewModelStructsMap(),
+	}
+	e.Parent = e
+	var field CalculateField
+	if f, ok := FieldByName(e, &field, "Children"); !ok || f.Relationship == nil {
+		t.Errorf("Should calculate fields correctly for the first time")
+	}
+}
