bc45fa170a5c5b0ab884cf77a36168c24aed3f03
gernest <geofreyernest@live.com>
2016-12-28 18:10:18 +0300

    [godoc] Add package description

--- ngorm.go
+++ ngorm.go
@@ -1,3 +1,34 @@
+//Package ngorm is a Go Object relation mapper that foucs on performance,
+//maintainability, modularity,	battle testing, extensibility , safety and
+//developer frindliness.
+//
+// To achieve all of the goals, the project is divided into many components. The
+// components are desined in a functional style API, whereby objects are
+// explicitly passed around as arguments to functions that operate on them.
+//
+// This tries to avoid defining methods on structs. This comes at a cost of
+// limiting chaining, this cost is intential. I intend to work really hard on
+// improving perfomance and thus avoiding spaghetti is not an option.
+//
+// The package is divided into two phases, Query building and Query execution
+// phase.
+//
+// Query Building
+//
+// The subpackage engine exposes a structure named Engine. This structure has
+// everything necessary to build a query. Most of the functions defined in this
+// package subpackages operate on this struct by accepting it as the first
+// argument.
+//
+// Having this as a separate layer helps fine tuning the generated querries and
+// also it make easy to test and very that the ORM is doing the right thing. So,
+// the generated query can be easily optimised without adding a lot of overhead.
+//
+// Query execution
+//
+// This is where the generated sql qery is executed. This phase is as generic as
+// possible in a way that you can easily implement adoptes for non SQL database
+// and still reap all the benefits of this package.
 package ngorm
 
 // DB contains information for current db connection
