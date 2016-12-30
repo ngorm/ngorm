3a28ded3236e62029ed2775358c02edaeaa171cf
gernest <geofreyernest@live.com>
2016-12-30 03:25:12 +0300

    [dialects] Add basic ql dialect implementation.

---
+++ dialects/ql/dialect.go
@@ -0,0 +1,160 @@
+package ql
+
+import (
+	"database/sql"
+	"fmt"
+	"math/big"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/gernest/ngorm/model"
+)
+
+type QL struct {
+	name string
+	db   *sql.DB
+}
+
+func Memory() *QL {
+	return &QL{name: "ql-mem"}
+}
+
+func File() *QL {
+	return &QL{name: "ql"}
+}
+
+// GetName get dialect's name
+func (q *QL) GetName() string {
+	return q.name
+}
+
+// SetDB set db for dialect
+func (q *QL) SetDB(db *sql.DB) {
+	q.db = db
+}
+
+// BindVar return the placeholder for actual values in SQL statements, in many dbs it is "?", Postgres using $1
+func (q QL) BindVar(i int) string {
+	return fmt.Sprintf("$%d", i)
+}
+
+// Quote quotes field name to avoid SQL parsing exceptions by using a reserved word as a field name
+func (a *QL) Quote(key string) string {
+	return fmt.Sprintf(`"%s"`, key)
+}
+
+// DataTypeOf return data's sql type
+func (q *QL) DataTypeOf(field *model.StructField) string {
+	var dataValue, sqlType, _, additionalType = model.ParseFieldStructForDialect(field)
+
+	if sqlType == "" {
+		switch dataValue.Kind() {
+		case reflect.Bool:
+			sqlType = "boolean"
+		case reflect.Int,
+			reflect.Int8,
+			reflect.Int16,
+			reflect.Int32,
+			reflect.Int64,
+			reflect.Uint,
+			reflect.Uint8,
+			reflect.Uint16,
+			reflect.Uint32,
+			reflect.Uint64,
+			reflect.Float32,
+			reflect.Float64,
+			reflect.String:
+			sqlType = dataValue.Kind().String()
+		case reflect.Struct:
+			switch dataValue.Interface().(type) {
+			case time.Time:
+				sqlType = "time"
+			case big.Int:
+				sqlType = "bigint"
+			case big.Rat:
+				sqlType = "bigrat"
+			}
+		default:
+			if _, ok := dataValue.Interface().([]byte); ok {
+				sqlType = "blob"
+			}
+		}
+	}
+
+	if sqlType == "" {
+		panic(fmt.Sprintf("invalid sql type %s (%s) for ql", dataValue.Type().Name(), dataValue.Kind().String()))
+	}
+
+	if strings.TrimSpace(additionalType) == "" {
+		return sqlType
+	}
+	return fmt.Sprintf("%v %v", sqlType, additionalType)
+}
+
+// HasIndex check has index or not
+func (q *QL) HasIndex(tableName string, indexName string) bool {
+	querry := "select count() from __Index where Name=$1  && TableName=$2"
+	var count int
+	q.db.QueryRow(querry, indexName, tableName).Scan(&count)
+	return count > 0
+}
+
+// HasForeignKey check has foreign key or not
+func (q *QL) HasForeignKey(tableName string, foreignKeyName string) bool {
+	return false
+}
+
+// RemoveIndex remove index
+func (q *QL) RemoveIndex(tableName string, indexName string) error {
+	tx, err := q.db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(fmt.Sprintf("DROP INDEX %v", indexName))
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+// HasTable check has table or not
+func (q *QL) HasTable(tableName string) bool {
+	querry := "select count() from __Table where Name=$1"
+	var count int
+	q.db.QueryRow(querry, tableName).Scan(&count)
+	return count > 0
+}
+
+// HasColumn check has column or not
+func (q *QL) HasColumn(tableName string, columnName string) bool {
+	querry := "select count() from __Column where Name=$1  && TableName=$2"
+	var count int
+	q.db.QueryRow(querry, columnName, tableName).Scan(&count)
+	return count > 0
+}
+
+// LimitAndOffsetSQL return generated SQL with Limit and Offset, as mssql has special case
+func (q *QL) LimitAndOffsetSQL(limit, offset interface{}) string {
+	return ""
+}
+
+// SelectFromDummyTable return select values, for most dbs, `SELECT values` just works, mysql needs `SELECT value FROM DUAL`
+func (q *QL) SelectFromDummyTable() string {
+	return ""
+}
+
+// LastInsertIdReturningSuffix most dbs support LastInsertId, but postgres needs to use `RETURNING`
+func (q *QL) LastInsertIDReturningSuffix(tableName, columnName string) string {
+	return ""
+}
+
+// BuildForeignKeyName returns a foreign key name for the given table, field and reference
+func (q *QL) BuildForeignKeyName(tableName, field, dest string) string {
+	return ""
+}
+
+// CurrentDatabase return current database name
+func (q *QL) CurrentDatabase() string {
+	return ""
+}
---
+++ dialects/ql/fialect_test.go
@@ -0,0 +1,75 @@
+package ql
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/cznic/ql/driver"
+)
+
+type Department struct {
+	ID   int
+	Name string
+}
+
+const migration = `
+BEGIN TRANSACTION;
+	CREATE TABLE Orders (CustomerID int, Date time);
+	CREATE INDEX OrdersID ON Orders (id());
+	CREATE INDEX OrdersDate ON Orders (Date);
+	CREATE TABLE Items (OrderID int, ProductID int, Qty int);
+	CREATE INDEX ItemsOrderID ON Items (OrderID);
+COMMIT;
+`
+
+func TestDialect(t *testing.T) {
+	dialect := Memory()
+	if dialect.GetName() != "ql-mem" {
+		t.Errorf("expected ql-mem got %s", dialect.GetName())
+	}
+	dialect = File()
+	if dialect.GetName() != "ql" {
+		t.Errorf("expected ql got %s", dialect.GetName())
+	}
+	db, err := sql.Open("ql-mem", "test.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx.Exec(migration)
+	tx.Commit()
+	dialect.SetDB(db)
+	if dialect.db == nil {
+		t.Fatal("expected the database to be set")
+	}
+
+	//
+	// HasIndex
+	//
+	if !dialect.HasIndex("Orders", "OrdersID") {
+		t.Error("expected to be true")
+	}
+
+	//RemoveIndex
+	err = dialect.RemoveIndex("Orders", "OrdersID")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if dialect.HasIndex("Orders", "OrdersID") {
+		t.Error("expected to be fasle")
+	}
+
+	// Has Table
+	if !dialect.HasTable("Orders") {
+		t.Error("expected to be true")
+	}
+	if !dialect.HasColumn("Orders", "Date") {
+		t.Error("expected to be true")
+	}
+
+}
