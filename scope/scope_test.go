edb5ff15faca805fdff86d71c3baf87fe5a78d05
gernest <geofreyernest@live.com>
2016-12-30 03:40:55 +0300

    [scope] Add TestQuote

--- scope/scope_test.go
+++ scope/scope_test.go
@@ -3,6 +3,7 @@ package scope
 import (
 	"testing"
 
+	"github.com/gernest/ngorm/dialects/ql"
 	"github.com/gernest/ngorm/fixture"
 )
 
@@ -12,5 +13,24 @@ func TestFieldByName(t *testing.T) {
 	var field fixture.CalculateField
 	if f, ok := FieldByName(e, &field, "Children"); !ok || f.Relationship == nil {
 		t.Errorf("Should calculate fields correctly for the first time")
+	}
+}
+
+func TestQuote(t *testing.T) {
+	e := fixture.TestEngine()
+	e.Dialect = &ql.QL{}
+	e.Parent = e
+	sample := []struct {
+		src, expetc string
+	}{
+		{"quote", `"quote"`},
+		{"quote.quote.quote", `"quote"."quote"."quote"`},
+	}
+
+	for _, v := range sample {
+		q := Quote(e, v.src)
+		if q != v.expetc {
+			t.Errorf("expected %s got %s", v.expetc, q)
+		}
 	}
 }
