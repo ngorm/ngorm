a6ff05337046b2d92536c9500f5fa70e1978eb3f
gernest <geofreyernest@live.com>
2016-12-31 15:58:18 +0300

    [regexes] Add more regexes

--- regexes/regexes.go
+++ regexes/regexes.go
@@ -2,4 +2,10 @@ package regexes
 
 import "regexp"
 
-var DistinctSQL = regexp.MustCompile(`(?i)distinct[^a-z]+[a-z]+`)
+var (
+	DistinctSQL    = regexp.MustCompile(`(?i)distinct[^a-z]+[a-z]+`)
+	Column         = regexp.MustCompile("^[a-zA-Z]+(\\.[a-zA-Z]+)*$") // only match string like `name`, `users.name`
+	IsNumber       = regexp.MustCompile("^\\s*\\d+\\s*$")             // match if string is number
+	Comparison     = regexp.MustCompile("(?i) (=|<>|>|<|LIKE|IS|IN) ")
+	CcountingQuery = regexp.MustCompile("(?i)^count(.+)$")
+)
