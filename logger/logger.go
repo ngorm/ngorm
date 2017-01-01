3a0e5c05323ebedef5bab16170d1210566c6b028
gernest <geofreyernest@live.com>
2017-01-01 22:40:53 +0300

    [logger] Add godoc

--- logger/logger.go
+++ logger/logger.go
@@ -1,5 +1,8 @@
+// Package logger defines interface for logging and provide a reference
+// implementation.
 package logger
 
+// Logger is an interface for logging.
 type Logger interface {
 	Print(v ...interface{})
 	Println(v ...interface{})
