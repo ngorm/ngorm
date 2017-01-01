4b5dc793a67e47438e75bb3023d23be14e00ac21
gernest <geofreyernest@live.com>
2017-01-01 22:16:04 +0300

    [engine] Add godoc

--- engine/db.go
+++ engine/db.go
@@ -1,3 +1,4 @@
+// Package engine  defines the core structure that drives the ngorm API.
 package engine
 
 import (
@@ -7,7 +8,8 @@ import (
 	"github.com/gernest/ngorm/model"
 )
 
-// DB contains information for current db connection
+//Engine is the driving force for ngorm. It contains, Scope, Search and other
+//utility properties for easily building complex SQL queries.
 type Engine struct {
 	Value             interface{}
 	Error             error
@@ -24,23 +26,22 @@ type Engine struct {
 	Search    *model.Search
 	Scope     *model.Scope
 	StructMap *model.SafeModelStructsMap
-
-	SQL *SQL
 }
 
-type SQL struct {
-	Query  string
-	Values []interface{}
-}
-
+//AddError adds err to Engine.Error.
+//
+// THis is here until I refactor all the APIs to return errors instead of
+// patching the Engine with arbitrary erros
 func (e *Engine) AddError(err error) error {
 	return nil
 }
 
+//DbTabler is an interface for getting database table name from the *Engine
 type DbTabler interface {
 	TableName(*Engine) string
 }
 
+//Tabler interface for defining table name
 type Tabler interface {
 	TableName() string
 }
