871089f007585ecea5c7432781b521c0c78ccbc6
gernest <geofreyernest@live.com>
2017-01-02 11:38:45 +0300

    [search] Fix lint errors

--- search/search.go
+++ search/search.go
@@ -9,24 +9,24 @@ import (
 	"github.com/gernest/ngorm/util"
 )
 
-func Where(e *engine.Engine, query interface{}, values ...interface{}) *engine.Engine {
+//Where adds WHERE search condition.
+func Where(e *engine.Engine, query interface{}, values ...interface{}) {
 	e.Search.WhereConditions = append(e.Search.WhereConditions, map[string]interface{}{"query": query, "args": values})
-	return e
 }
 
-func Not(e *engine.Engine, query interface{}, values ...interface{}) *engine.Engine {
+//Not adds NOT search condition
+func Not(e *engine.Engine, query interface{}, values ...interface{}) {
 	e.Search.NotConditions = append(e.Search.NotConditions, map[string]interface{}{"query": query, "args": values})
-	return e
 }
 
-func Or(e *engine.Engine, query interface{}, values ...interface{}) *engine.Engine {
+//Or add OR search condition
+func Or(e *engine.Engine, query interface{}, values ...interface{}) {
 	e.Search.OrConditions = append(e.Search.OrConditions, map[string]interface{}{"query": query, "args": values})
-	return e
 }
 
-func Attr(e *engine.Engine, attrs ...interface{}) *engine.Engine {
+//Attr add attributes
+func Attr(e *engine.Engine, attrs ...interface{}) {
 	e.Search.InitAttrs = append(e.Search.InitAttrs, toSearchableMap(attrs...))
-	return e
 }
 
 func toSearchableMap(attrs ...interface{}) (result interface{}) {
@@ -46,12 +46,13 @@ func toSearchableMap(attrs ...interface{}) (result interface{}) {
 	return
 }
 
-func Assign(e *engine.Engine, attrs ...interface{}) *engine.Engine {
+//Assign assigns attrs
+func Assign(e *engine.Engine, attrs ...interface{}) {
 	e.Search.AssignAttrs = append(e.Search.AssignAttrs, toSearchableMap(attrs...))
-	return e
 }
 
-func Order(e *engine.Engine, value interface{}, reorder ...bool) *engine.Engine {
+//Order add ORDER BY search condition
+func Order(e *engine.Engine, value interface{}, reorder ...bool) {
 	if len(reorder) > 0 && reorder[0] {
 		e.Search.Orders = []interface{}{}
 	}
@@ -59,75 +60,74 @@ func Order(e *engine.Engine, value interface{}, reorder ...bool) *engine.Engine 
 	if value != nil {
 		e.Search.Orders = append(e.Search.Orders, value)
 	}
-	return e
 }
 
-func Select(e *engine.Engine, query interface{}, values ...interface{}) *engine.Engine {
+//Select add SELECT querry
+func Select(e *engine.Engine, query interface{}, values ...interface{}) {
 	if regexes.DistinctSQL.MatchString(fmt.Sprint(query)) {
 		e.Search.IgnoreOrderQuery = true
 	}
 	e.Search.Selects = map[string]interface{}{"query": query, "args": values}
-	return e
 }
 
-func Omit(e *engine.Engine, columns ...string) *engine.Engine {
+//Omit ommits seacrh condition
+func Omit(e *engine.Engine, columns ...string) {
 	e.Search.Omits = columns
-	return e
 }
 
-func Limit(e *engine.Engine, limit interface{}) *engine.Engine {
+//Limit add search LIMIT
+func Limit(e *engine.Engine, limit interface{}) {
 	e.Search.Limit = limit
-	return e
 }
 
-func Offset(e *engine.Engine, offset interface{}) *engine.Engine {
+//Offset add search OFFSET
+func Offset(e *engine.Engine, offset interface{}) {
 	e.Search.Offset = offset
-	return e
 }
 
-func Group(e *engine.Engine, query interface{}) *engine.Engine {
+//Group  add GROUP BY search condition.
+func Group(e *engine.Engine, query interface{}) {
 	s, err := util.GetInterfaceAsSQL(query)
 	if err != nil {
 		e.Error = err
 	} else {
 		e.Search.Group = s
 	}
-	return e
 }
 
-func Having(e *engine.Engine, query interface{}, values ...interface{}) *engine.Engine {
+//Having add HAVING condition
+func Having(e *engine.Engine, query interface{}, values ...interface{}) {
 	e.Search.HavingConditions = append(e.Search.HavingConditions, map[string]interface{}{"query": query, "args": values})
-	return e
 }
 
-func Join(e *engine.Engine, query interface{}, values ...interface{}) *engine.Engine {
+//Join add JOIN condition
+func Join(e *engine.Engine, query interface{}, values ...interface{}) {
 	e.Search.JoinConditions = append(e.Search.JoinConditions, map[string]interface{}{"query": query, "args": values})
-	return e
 }
 
-func Preload(e *engine.Engine, schema string, values ...interface{}) *engine.Engine {
+//Preload add preloading condition
+func Preload(e *engine.Engine, schema string, values ...interface{}) {
 	var preloads []model.SearchPreload
 	for _, preload := range e.Search.Preload {
 		if preload.Schema != schema {
 			preloads = append(preloads, preload)
 		}
 	}
-	preloads = append(preloads, model.SearchPreload{schema, values})
+	preloads = append(preloads, model.SearchPreload{Schema: schema, Conditions: values})
 	e.Search.Preload = preloads
-	return e
 }
 
-func Raw(e *engine.Engine, b bool) *engine.Engine {
+//Raw set the seacrh querry to RAw
+func Raw(e *engine.Engine, b bool) {
 	e.Search.Raw = b
-	return e
 }
 
-func Unscoped(e *engine.Engine, b bool) *engine.Engine {
+//Unscoped set the search scope status
+func Unscoped(e *engine.Engine, b bool) {
 	e.Search.Unscoped = b
-	return e
 }
 
-func Table(e *engine.Engine, name string) *engine.Engine {
+//Table set the search table name.
+func Table(e *engine.Engine, name string) {
 	e.Search.TableName = name
-	return e
 }
