c98a0130d6aa67beb6e4c0112e6516612ca9b2a4
gernest <geofreyernest@live.com>
2017-01-06 14:52:09 +0300

    [hooks] Add Create

--- hooks/default_hooks.go
+++ hooks/default_hooks.go
@@ -4,9 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/gernest/ngorm/builder"
 	"github.com/gernest/ngorm/engine"
+	"github.com/gernest/ngorm/errmsg"
 	"github.com/gernest/ngorm/model"
 	"github.com/gernest/ngorm/scope"
 	"github.com/gernest/ngorm/search"
@@ -103,6 +105,122 @@ func BeforeCreate(b *HooksBook, e *engine.Engine) error {
 		err := bc.Exec(b, e)
 		if err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+func Create(b *HooksBook, e *engine.Engine) error {
+	var (
+		columns, placeholders []string
+
+		// The blank columns with default values
+		cv []string
+	)
+	fds, err := scope.Fields(e, e.Scope.Value)
+	if err != nil {
+		return err
+	}
+
+	for _, field := range fds {
+		if scope.ChangeableField(e, field) {
+			if field.IsNormal {
+				if field.IsBlank && field.HasDefaultValue {
+					cv = append(cv, scope.Quote(e, field.DBName))
+					e.Scope.Set("gorm:blank_columns_with_default_value", cv)
+				} else if !field.IsPrimaryKey || !field.IsBlank {
+					columns = append(columns, scope.Quote(e, field.DBName))
+					placeholders = append(placeholders, scope.AddToVars(e, field.Field.Interface()))
+				}
+			} else if field.Relationship != nil && field.Relationship.Kind == "belongs_to" {
+				for _, foreignKey := range field.Relationship.ForeignDBNames {
+					foreignField, err := scope.FieldByName(e, e.Scope.Value, foreignKey)
+					if err != nil {
+						return err
+					} else {
+						if !scope.ChangeableField(e, foreignField) {
+							columns = append(columns, scope.Quote(e, foreignField.DBName))
+							placeholders = append(placeholders, scope.AddToVars(e, foreignField.Field.Interface()))
+						}
+					}
+
+				}
+			}
+		}
+	}
+
+	var (
+		returningColumn = "*"
+		tableName       = scope.QuotedTableName(e, e.Scope.Value)
+
+		extraOption string
+	)
+
+	primaryField, err := scope.PrimaryField(e, e.Scope.Value)
+	if err != nil {
+		return err
+	}
+	if str, ok := e.Scope.Get("gorm:insert_option"); ok {
+		extraOption = fmt.Sprint(str)
+	}
+
+	if primaryField != nil {
+		returningColumn = scope.Quote(e, primaryField.DBName)
+	}
+
+	lastInsertIDReturningSuffix :=
+		e.Dialect.LastInsertIDReturningSuffix(tableName, returningColumn)
+
+	if len(columns) == 0 {
+		sql := fmt.Sprintf(
+			"INSERT INTO %v DEFAULT VALUES%v%v",
+			tableName,
+			util.AddExtraSpaceIfExist(extraOption),
+			util.AddExtraSpaceIfExist(lastInsertIDReturningSuffix),
+		)
+		e.Scope.SQL = strings.Replace(sql, "$$", "?", -1)
+	} else {
+		sql := fmt.Sprintf(
+			"INSERT INTO %v (%v) VALUES (%v)%v%v",
+			scope.QuotedTableName(e, e.Scope.Value),
+			strings.Join(columns, ","),
+			strings.Join(placeholders, ","),
+			util.AddExtraSpaceIfExist(extraOption),
+			util.AddExtraSpaceIfExist(lastInsertIDReturningSuffix),
+		)
+		e.Scope.SQL = strings.Replace(sql, "$$", "?", -1)
+	}
+
+	// execute create sql
+	if lastInsertIDReturningSuffix == "" || primaryField == nil {
+		result, err := e.SQLDB.Exec(e.Scope.SQL, e.Scope.SQLVars...)
+		if err != nil {
+			return err
+		}
+		// set rows affected count
+		e.RowsAffected, _ = result.RowsAffected()
+
+		// set primary value to primary field
+		if primaryField != nil && primaryField.IsBlank {
+			primaryValue, err := result.LastInsertId()
+			if err != nil {
+				return err
+			}
+			primaryField.Set(primaryValue)
+		}
+	} else {
+		if primaryField.Field.CanAddr() {
+			err := e.SQLDB.QueryRow(
+				e.Scope.SQL,
+				e.Scope.SQLVars...,
+			).Scan(primaryField.Field.Addr().Interface())
+			if err != nil {
+				return err
+			}
+			primaryField.IsBlank = false
+			e.RowsAffected = 1
+		} else {
+			return errmsg.ErrUnaddressable
 		}
 	}
 	return nil
