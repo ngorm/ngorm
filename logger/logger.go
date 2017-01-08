6f5db7c567ba5eebf41d79fa7205bd91e9adc4a2
gernest <geofreyernest@live.com>
2017-01-08 22:32:24 +0300

    [logger] Lint Zapper

--- logger/logger.go
+++ logger/logger.go
@@ -13,6 +13,7 @@ type Logger interface {
 	zap.Logger
 }
 
+//Zapper is the ngorm logger that is based on uber/zap package.
 type Zapper struct {
 	start    time.Time
 	withTime bool
@@ -20,24 +21,33 @@ type Zapper struct {
 	log      Logger
 }
 
+//New return new Zapper instance with the l set as the default logger.
 func New(l Logger) *Zapper {
 	return &Zapper{
 		log: l,
 	}
 }
 
+//Start return Zapper instance
 func (z *Zapper) Start() *Zapper {
 	return &Zapper{
 		log: z.log,
 	}
 }
+
+//StartWithTime  return Zapper instance with start time set to now. This is useful if you
+//want to tract duration of a certain event.
+//
+// Any log methof called on the returned istance will record the duration.
 func (z *Zapper) StartWithTime() *Zapper {
 	return &Zapper{
-		start: time.Now(),
-		log:   z.log,
+		start:    time.Now(),
+		log:      z.log,
+		withTime: true,
 	}
 }
 
+// Log logs val with level or fields.
 func (z *Zapper) Log(level zap.Level, val string, fields ...zap.Field) {
 	if z.withTime {
 		now := time.Now()
@@ -54,17 +64,22 @@ func (z *Zapper) Log(level zap.Level, val string, fields ...zap.Field) {
 	z.log.Log(level, val, f...)
 }
 
+//Info logs with level set to info
 func (z *Zapper) Info(arg string, fields ...zap.Field) {
 	z.Log(zap.InfoLevel, arg, fields...)
 }
 
+// Debug logs with level set to debug
 func (z *Zapper) Debug(arg string, fields ...zap.Field) {
 	z.Log(zap.InfoLevel, arg, fields...)
 }
 
-func (z *Zapper) Fatal(arg string, fields ...zap.Field) {
-	z.Log(zap.FatalLevel, arg, fields...)
-}
+//Warn logs warnings
 func (z *Zapper) Warn(arg string, fields ...zap.Field) {
 	z.Log(zap.WarnLevel, arg, fields...)
+}
+
+//Fields Add fiels
+func (z *Zapper) Fields(f ...zap.Field) {
+	z.fiedls = append(z.fiedls, f...)
 }
