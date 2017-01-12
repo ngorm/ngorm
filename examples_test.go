c4aa0ad67dbbe7d57b1a484d53a472991e7e0bbc
gernest <geofreyernest@live.com>
2017-01-12 12:13:28 +0300

    [ngorm] Add ExampleDB_CreateSQL

---
+++ examples_test.go
@@ -0,0 +1,31 @@
+package ngorm
+
+import "fmt"
+
+func ExampleDB_CreateSQL() {
+	db, err := Open("ql-mem", "test.db")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		defer func() { _ = db.Close() }()
+		type Bar struct {
+			ID  int64
+			Say string
+		}
+
+		b := Bar{Say: "hello"}
+		sql, err := db.CreateSQL(&b)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(sql.Q)
+			fmt.Printf("$1=%v", sql.Args[0])
+		}
+	}
+
+	//Output:
+	//BEGIN TRANSACTION;
+	//	INSERT INTO bars (say) VALUES ($1);
+	//COMMIT;
+	//$1=hello
+}
