eb340149e783d8f481a6702434d4b9b8466d9eea
gernest <geofreyernest@live.com>
2017-01-13 15:55:20 +0300

    [ngorm] Halt examples execution on error

--- examples_test.go
+++ examples_test.go
@@ -11,34 +11,31 @@ import (
 func ExampleOpen() {
 	db, err := Open("ql-mem", "test.db")
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		defer func() { _ = db.Close() }()
-		fmt.Println(db.Dialect().GetName())
+		log.Fatal(err)
 	}
+	defer func() { _ = db.Close() }()
+	fmt.Println(db.Dialect().GetName())
 
 	//Output:ql-mem
 }
 func ExampleDB_CreateSQL() {
 	db, err := Open("ql-mem", "test.db")
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		defer func() { _ = db.Close() }()
-		type Bar struct {
-			ID  int64
-			Say string
-		}
-
-		b := Bar{Say: "hello"}
-		sql, err := db.CreateSQL(&b)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(sql.Q)
-			fmt.Printf("$1=%v", sql.Args[0])
-		}
+		log.Fatal(err)
 	}
+	defer func() { _ = db.Close() }()
+	type Bar struct {
+		ID  int64
+		Say string
+	}
+
+	b := Bar{Say: "hello"}
+	sql, err := db.CreateSQL(&b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sql.Q)
+	fmt.Printf("$1=%v", sql.Args[0])
 
 	//Output:
 	//BEGIN TRANSACTION;
@@ -50,27 +47,25 @@ func ExampleDB_CreateSQL() {
 func ExampleDB_AutomigrateSQL() {
 	db, err := Open("ql-mem", "test.db")
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		defer func() { _ = db.Close() }()
-
-		type Bar struct {
-			ID  int64
-			Say string
-		}
-
-		type Bun struct {
-			ID   int64
-			Dead bool
-		}
-
-		sql, err := db.AutomigrateSQL(&Bar{}, &Bun{})
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(sql.Q)
-		}
+		log.Fatal(err)
 	}
+	defer func() { _ = db.Close() }()
+
+	type Bar struct {
+		ID  int64
+		Say string
+	}
+
+	type Bun struct {
+		ID   int64
+		Dead bool
+	}
+
+	sql, err := db.AutomigrateSQL(&Bar{}, &Bun{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sql.Q)
 
 	//Output:
 	//BEGIN TRANSACTION;
@@ -82,49 +77,46 @@ func ExampleDB_AutomigrateSQL() {
 func ExampleDB_Automigrate() {
 	db, err := Open("ql-mem", "test.db")
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		defer func() { _ = db.Close() }()
+		log.Fatal(err)
+	}
+	defer func() { _ = db.Close() }()
 
-		type Bar struct {
-			ID  int64
-			Say string
-		}
+	type Bar struct {
+		ID  int64
+		Say string
+	}
 
-		type Bun struct {
-			ID   int64
-			Dead bool
-		}
+	type Bun struct {
+		ID   int64
+		Dead bool
+	}
 
-		_, err = db.Automigrate(&Bar{}, &Bun{})
+	_, err = db.Automigrate(&Bar{}, &Bun{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var names []string
+	qdb := db.SQLCommon()
+	rows, err := qdb.Query("select Name  from __Table ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() { _ = rows.Close() }()
+	for rows.Next() {
+		var n string
+		err = rows.Scan(&n)
 		if err != nil {
+			log.Fatal(err)
 			fmt.Println(err)
-		} else {
-			var names []string
-			qdb := db.SQLCommon()
-			rows, err := qdb.Query("select Name  from __Table ")
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				defer func() { _ = rows.Close() }()
-				for rows.Next() {
-					var n string
-					err = rows.Scan(&n)
-					if err != nil {
-						fmt.Println(err)
-					} else {
-						names = append(names, n)
-					}
-				}
-				if err := rows.Err(); err != nil {
-					log.Fatal(err)
-				}
-			}
-			sort.Strings(names)
-			for _, v := range names {
-				fmt.Println(v)
-			}
 		}
+		names = append(names, n)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	sort.Strings(names)
+	for _, v := range names {
+		fmt.Println(v)
 	}
 
 	//Output:
@@ -137,40 +129,37 @@ func Example_belongsTo() {
 	//One to many relationship
 	db, err := Open("ql-mem", "test.db")
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		defer func() { _ = db.Close() }()
-
-		// Here one user can only have one profile. But one profile can belong
-		// to multiple users.
-		type Profile struct {
-			model.Model
-			Name string
-		}
-		type User struct {
-			model.Model
-			Profile   Profile
-			ProfileID int
-		}
-		_, err = db.Automigrate(&User{}, &Profile{})
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			u := User{
-				Profile: Profile{Name: "gernest"},
-			}
-
-			// Creating the user with the Profile. The relation will
-			// automatically be created and the user.ProfileID will be set to
-			// the ID of hte created profile.
-			err = db.Create(&u)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Println(u.ProfileID == int(u.Profile.ID) && u.ProfileID != 0)
-			}
-		}
+		log.Fatal(err)
 	}
+	defer func() { _ = db.Close() }()
+
+	// Here one user can only have one profile. But one profile can belong
+	// to multiple users.
+	type Profile struct {
+		model.Model
+		Name string
+	}
+	type User struct {
+		model.Model
+		Profile   Profile
+		ProfileID int
+	}
+	_, err = db.Automigrate(&User{}, &Profile{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	u := User{
+		Profile: Profile{Name: "gernest"},
+	}
+
+	// Creating the user with the Profile. The relation will
+	// automatically be created and the user.ProfileID will be set to
+	// the ID of hte created profile.
+	err = db.Create(&u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(u.ProfileID == int(u.Profile.ID) && u.ProfileID != 0)
 
 	//Output:
 	//true
