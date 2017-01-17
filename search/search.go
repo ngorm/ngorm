d9cb12a18b2155473257335bedde63cd0e07dc53
gernest <geofreyernest@live.com>
2017-01-17 19:05:58 +0300

    [search] Remove duplicate code

--- search/search.go
+++ search/search.go
@@ -27,29 +27,12 @@ func Or(e *engine.Engine, query interface{}, values ...interface{}) {
 
 //Attr add attributes
 func Attr(e *engine.Engine, attrs ...interface{}) {
-	e.Search.InitAttrs = append(e.Search.InitAttrs, toSearchableMap(attrs...))
-}
-
-func toSearchableMap(attrs ...interface{}) (result interface{}) {
-	if len(attrs) > 1 {
-		if str, ok := attrs[0].(string); ok {
-			result = map[string]interface{}{str: attrs[1]}
-		}
-	} else if len(attrs) == 1 {
-		if attr, ok := attrs[0].(map[string]interface{}); ok {
-			result = attr
-		}
-
-		if attr, ok := attrs[0].(interface{}); ok {
-			result = attr
-		}
-	}
-	return
+	e.Search.InitAttrs = append(e.Search.InitAttrs, util.ToSearchableMap(attrs...))
 }
 
 //Assign assigns attrs
 func Assign(e *engine.Engine, attrs ...interface{}) {
-	e.Search.AssignAttrs = append(e.Search.AssignAttrs, toSearchableMap(attrs...))
+	e.Search.AssignAttrs = append(e.Search.AssignAttrs, util.ToSearchableMap(attrs...))
 }
 
 //Order add ORDER BY search condition
