afe0e6f2ecf1cd35603ebd09bb6995c343e7c177
gernest <geofreyernest@live.com>
2017-04-29 19:13:16 +0300

    [ngorm] update godoc

--- ngorm.go
+++ ngorm.go
@@ -1,40 +1,18 @@
-//Package ngorm i a Go Object relation mapper that focus on performance,
+//Package ngorm is a Go Object relation mapper that focus on performance,
 //maintainability, modularity,	battle testing, extensibility , safety and
 //developer friendliness.
 //
-// To achieve all of the goals, the project is divided into many components. The
-// components are designed in a functional style API, whereby objects are
-// explicitly passed around as arguments to functions that operate on them.
-//
-// This tries to avoid defining methods on structs. This comes at a cost of
-// limiting chaining, this cost is intentional. I intend to work really hard on
-// improving performance and thus avoiding spaghetti is not an option.
 //
 // Installation
 //
 // You can install  with go get
 //   go get -u github.com/ngorm/ngorm
 //
+// Supported databases
 //
-// The package is divided into two phases, Query building and Query execution
-// phase.
-//
-// Query Building
-//
-// The subpackage engine exposes a structure named Engine. This structure has
-// everything necessary to build a query. Most of the functions defined in this
-// package subpackages operate on this struct by accepting it as the first
-// argument.
-//
-// Having this as a separate layer helps fine tuning the generated queries and
-// also it make easy to test and very that the ORM is doing the right thing. So,
-// the generated query can be easily optimise without adding a lot of overhead.
-//
-// Query execution
-//
-// This is s the phase where the generated sql query is executed. This phase is as generic as
-// possible in a way that you can easily implement adopters for non SQL database
-// and still reap all the benefits of this package.
+//At the moment the following databases are supported
+//   - ql
+//   - postgresql
 //
 //
 package ngorm
